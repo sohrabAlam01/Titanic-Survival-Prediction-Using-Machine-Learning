2a4d4474b84d3cd47ebbd077088edfd8a41e84b9
sohrabAlam01 <144530218+sohrabAlam01@users.noreply.github.com>
2023-09-10 00:45:02 +0530

    Initial commit

---
+++ #52To60.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+
+class shop{
+	
+	private :
+		int id; float price;
+		public:
+			void setData(int a, float b){
+				id = a;
+				price = b;
+			}
+			void getData(void){
+				cout<<"the id is "<<id;
+				cout<<"the price is "<<price;
+			}
+};
+int main(){
+	
+	int size = 3;
+	shop *ptr = new shop[size];
+	shop *ptrTemp = ptr;
+	int p; float q;
+	for(int i = 1; i<=3; i++){
+		 
+		  cout<<"enter the id and price of itemnumber"<<i<<endl;
+		  cin>>p>>q;
+		  ptr->setData(p,q);
+		  ptr++;
+		
+	}
+	
+	for(int i=1; i<=3; i++){
+		
+		ptrTemp->getData();
+		ptrTemp++;
+	}
+	
+	return 0;
+}
